1e792559c90559c76d8295e6468e8604173c3b00
Nguyen Tien Sy <troioidungaigiongtoi4@gmail.com>
2025-09-19 11:29:22 +0700

    sua lan 3

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,31 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/auth/Login";
 import Dashboard from "../pages/dashboard/Index";
+import ProtectedRoute from "../components/layout/ProtectedRoute";
+import RoleGuard from "../components/layout/RoleGuard";
+
+function InternTasks() {
+  return <div style={{ padding: 24 }}>Intern - Tasks</div>;
+}
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route
+            path="/intern/tasks"
+            element={
+              <RoleGuard roles={["INTERN"]}>
+                <InternTasks />
+              </RoleGuard>
+            }
+          />
+        </Route>
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
