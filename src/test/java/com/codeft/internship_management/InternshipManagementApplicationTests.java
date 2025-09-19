c9ba5e2111e60d88aaef9034a060a8f0f6d7e985
Hckr2005 <phat14112005@gmail.com>
2025-09-19 10:26:11 +0700

    First commit

---
+++ src/test/java/com/codeft/internship_management/InternshipManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeft.internship_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
